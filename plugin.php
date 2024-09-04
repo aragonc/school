84a816913f6b8baa5eff3ca8f9dbf2e2dc712b67
Alex Aragon <aragcar@gmail.com>
2024-09-04 01:02:07 -0500

    add files

---
+++ plugin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Config the plugin.
+ *
+ * @author Alex Aragon <alex.aragon@tunqui.pe>
+ *
+ * @package chamilo.plugin.school
+ */
+$plugin_info = SchoolPlugin::create()->get_info();
---
+++ uninstall.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Config the plugin.
+ *
+ * @author Alex Aragon <alex.aragon@tunqui.pe>
+ *
+ * @package chamilo.plugin.school
+ */
+
+require_once __DIR__.'/config.php';
+
+SchoolPlugin::create()->uninstall();
