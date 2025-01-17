4710d80b1c3b48ed39d5eb7fe14da19bba7431e8
Alex Aragon <aragcar@gmail.com>
2025-01-16 19:01:34 -0500

    add photo html

--- avatar.php
+++ avatar.php
@@ -31,10 +31,45 @@ if ($enable) {
         [],
         FormValidator::LAYOUT_NEW
     );
+    $form->addHtml('<div class="form-group">');
+    $form->addHtml('<label class="form-label label">Vista previa de image</label>');
+    $form->addHtml('<div class="card avatar-profile">
+        <div class="card-body preview">
+            <img class="img-fluid img-circle" src="' . $user_data['avatar']. '"  alt=""/>
+    </div>
+    </div>');
+
+    $form->addFile(
+        'picture',
+        [
+            $user_data['picture_uri'] != '' ? get_lang('UpdateImage') : get_lang('AddImage'),
+            get_lang('OnlyImagesAllowed'),
+        ],
+        [
+            'id' => 'picture',
+            'class' => 'picture-form',
+            'crop_image' => true,
+            'crop_ratio' => '1 / 1',
+            'accept' => 'image/*',
+        ]
+    );
+
+    $form->addProgress();
+    if (!empty($user_data['picture_uri'])) {
+        $form->addElement('checkbox', 'remove_picture', null, get_lang('DelImage'));
+    }
+    $allowed_picture_types = api_get_supported_image_extensions(false);
+    $form->addRule(
+        'picture',
+        get_lang('OnlyImagesAllowed').' ('.implode(', ', $allowed_picture_types).')',
+        'filetype',
+        $allowed_picture_types
+    );
+
 
     $plugin->setTitle($plugin->get_lang('EditProfile'));
     $plugin->assign('img_section', $imgSection);
-    $plugin->assign('form', '');
+    $plugin->assign('form', $form->returnForm());
     $content = $plugin->fetch('school_avatar.tpl');
     $plugin->assign('content', $content);
     $plugin->display_blank_template();
