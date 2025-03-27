ea03b792415a374cdd90738767cca7a853291e7a
Alex Aragon <aragcar@gmail.com>
2025-03-27 17:39:07 -0500

    fix filter graduates

--- lang/spanish.php
+++ lang/spanish.php
@@ -100,6 +100,6 @@ $strings['ErrorForms'] = "Hay errores, corregir antes de enviar";
 $strings['SelectAnImage'] = "Seleccionar una imagen.";
 $strings['CourseCalendar'] = "Calendario del curso";
 $strings['DiplomaSchedule'] = "Calendario del diplomado";
-$strings['ShowByCategories'] = "Mostrar por categorias";
+$strings['ShowByCategories'] = "Filtrar por categorías";
 $strings['All'] = "Todos";
 $strings['Registrations'] = "Inscripciones";
